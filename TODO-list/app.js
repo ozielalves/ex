a88c2fe1d8e21dd7dc7c6e4f4f160f4af4bea801
Oziel <ozielalves@ufrn.edu.br>
2020-08-24 21:16:33 -0300

    some animation and js vanilla injection

--- TODO-list/app.js
+++ TODO-list/app.js
@@ -2,9 +2,19 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const header = document.querySelector('header');
+const todo_on = document.querySelector('todo');
+
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+// Removing big shadow
+todoButton.addEventListener('click', () => {
+    header.classList.add('not-blank');
+});
+todoList.addEventListener('click', deleteCheck);
+
+
 // Functions
 function addTodo(event) {
     // Prevent from submitting
@@ -26,8 +36,27 @@ function addTodo(event) {
     // Delete button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn');
+    trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
     // Append to Todo List
     todoList.appendChild(todoDiv);
+    // Clear Todo INPUT VALUE
+    todoInput.value = "";
 };
+
+function deleteCheck(e) {
+    const item = e.target;
+    //DELTE TODO
+    if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        // The Event Animation
+        todo.classList.toggle('fadeOut');
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        }); 
+    }
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
